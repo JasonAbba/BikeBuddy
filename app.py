e0a0c7707f7c3bf789b907bb72117a14c0040f28
Jason Abba <53237259+JasonAbba@users.noreply.github.com>
2023-12-02 01:48:45 +0530

    Update app.py
    Ride code was not displaying after button click

--- app.py
+++ app.py
@@ -377,6 +377,7 @@ def tab3_content():
                 
                 user = st.session_state['user_id']
                 db.child("--Created Rides--").child(ride_code).set(ride_details)
+                st.info('Your Ride Code: ', ride_code)
                 st.toast('A ride has been created succesfully!', icon='✅')
                 
     with col2:
